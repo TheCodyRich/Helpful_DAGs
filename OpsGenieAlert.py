5a18e6fda72987482d69ba74be89b2d30e8947da
Cody Rich <83995939+TheCodyRich@users.noreply.github.com>
2024-05-10 14:23:25 -0400

    Update OpsGenieAlert.py

--- OpsGenieAlert.py
+++ OpsGenieAlert.py
@@ -1,3 +1,9 @@
+# USAGE: A simple dag that can be triggered by Astro Alerts (Trigger Dag) to create an OpsGenie Alert.  The DAG will grab the error message and DAG
+# name from the conf and then insert it into the OpsGenie Alert
+# 
+# There are no guarantees associated with this DAG
+
+
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -12,11 +18,18 @@ def get_formatted_message(**context):
     formatted_message = f"Error: {dag_name} failed for this reason: {message}"
     return formatted_message
 
+default_args = {
+    'owner': 'Cody',
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
 with DAG(
     dag_id="opsgenie_alert_dag",
     start_date=datetime(2024, 4, 1),
     schedule_interval=None,
     catchup=False,
+    default_args=default_args,
 ) as dag:
 
     get_formatted_message_task = PythonOperator(
